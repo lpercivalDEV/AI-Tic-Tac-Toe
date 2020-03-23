35b45c1753f1b400b8ebbf29dfec34090b479e6a
lpercivalDEV <lpercival.dev@gmail.com>
2020-03-23 16:47:22 -0400

    Create minimax algorithm for ai plays

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function startGame() {
   originalBrd = Array.from(Array(9).keys());
   for (let i=0; i < cells.length; i++) {
     cells[i].innerText = "";
+    cells[i].style.color = "lime";
     cells[i].style.removeProperty("background-color");
     cells[i].addEventListener("click", turnClick, false);
   }
@@ -42,7 +43,6 @@ function turn(squareId, player) {
   originalBrd[squareId] = player;
   document.getElementById(squareId).innerText = player;
   let gameWin = checkWin(originalBrd, player);
-  console.log(gameWin, player);
   if(gameWin){
     gameOver(gameWin);
   }
@@ -63,12 +63,85 @@ function checkWin(board, player) {
 
 function gameOver(gameWin) {
   for (let index of winCombos [gameWin.index]) {
-    console.log(index);
     document.getElementById(index).style.backgroundColor = "white";
-    document.getElementById(index).style.color = gameWin.player == human ? "lime" : "red";
+    document.getElementById(index).style.color = gameWin.player == human ? "blue" : "red";
   }
-  for (let i = 0; i<cells.length; i++) {
+  for (let i = 0; i< cells.length; i++) {
     cells[i].removeEventListener("click", turnClick, false);
   }
-  declareWinner(gameWin.player == human ? "You win!" : "You did not win.");
+  declareWinner(gameWin.player == human ? "You win!" : "You lose.");
+}
+
+function declareWinner(who) {
+  document.querySelector(".endgame").style.display = "inline-block";
+  document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+  return originalBrd.filter(square => typeof square == "number");
+}
+
+function bestChoice() {
+  return minimax(originalBrd, ai).index;
+}
+
+function checkTie() {
+  if (emptySquares().length == 0) {
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "green";
+      cells[i].removeEventListener("click", turnClick, false);
+    }
+    declareWinner("Tie Game.");
+    return true;
+  }
+  return false;
+}
+
+function minimax(newBoard, player) {
+  let openSq = emptySquares(newBoard);
+
+  if (checkWin(newBoard, human)) {
+    return {score: -10};
+  }else if (checkWin(newBoard, ai)) {
+    return {score: 10};
+  }else if (openSq.length === 0) {
+    return {score: 0};
+  }
+  let plays = [];
+  for (let i = 0; i < openSq.length; i++) {
+    let play = {};
+    play.index = newBoard[openSq[i]];
+    newBoard[openSq[i]] = player;
+
+    //recursion - continue until terminal state is found for one of the players and update the resulting board/score property
+    if (player == ai) {
+      let result = minimax(newBoard, human);
+      play.score = result.score;
+    }else {
+      let result = minimax(newBoard, ai);
+      play.score = result.score;
+    }
+    newBoard[openSq[i]] = play.index;
+
+    plays.push(play);
+  }
+  let bestPlay;
+  if(player == ai) {
+    let bestScore = -10000;
+    for (let i = 0; i<plays.length; i++) {
+      if (plays[i].score > bestScore) {
+        bestScore = plays[i].score;
+        bestPlay = i;
+      }
+    }
+  }else {
+    let bestScore = 10000;
+    for (let i = 0; i<plays.length; i++) {
+      if (plays[i].score < bestScore) {
+        bestScore = plays[i].score;
+        bestPlay = i;
+      }
+    }
+  }
+  return plays[bestPlay];
 }
